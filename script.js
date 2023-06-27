46c0f2aa0c5bd74284d78c1366e240f76647536c
ayindesadiq01 <ayindesadiq01@gmail.com>
2023-06-27 12:10:04 +0100

    init

---
+++ script.js
@@ -0,0 +1,82 @@
+'use strict';
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+const c_icon = document.querySelector('.cicon');
+const navBar = document.querySelector('.nav-bar');
+const blurry = document.querySelector('.blurry');
+
+
+menuIcon.addEventListener('click', () => {
+ closeIcon.classList.remove('cicon');
+ menuIcon.classList.add('cicon');
+ navBar.classList.remove('hidden')
+ blurry.style.display = 'block';
+})
+closeIcon.addEventListener('click', () => {
+ menuIcon.classList.remove('cicon');
+ closeIcon.classList.add('cicon');
+ navBar.classList.add('hidden')
+ blurry.style.display = 'none';
+
+})
+
+// WORKING WITH NAV BAR
+const navList = document.querySelector('.nav-list');
+
+navList.addEventListener('click', (e) => {
+ e.preventDefault();
+ const clicked = e.target.classList.contains('nav-link');
+ console.log(clicked);
+ if(clicked) {
+ const lick = e.target.getAttribute('href');
+ document.querySelector(lick).scrollIntoView({
+  behavior: 'smooth'
+ })
+ }
+})
+
+// WORKING WITH STICKY NAV BAR using intersection observer
+const sectionOne = document.querySelector('#section-one');
+const sectionTwo = document.querySelector('#section-two');
+const topBar =document.querySelector('.top');
+const navLink = document.querySelectorAll('.nav-link');
+
+
+const topHeight = topBar.getBoundingClientRect().height;
+const easyFtn = (entries) => {
+ const [entry] =entries;
+ // console.log(entry);
+ if(!entry.isIntersecting) {
+  topBar.classList.add('sticky');
+ } else {
+  topBar.classList.remove('sticky');
+ }
+}
+
+const easyObserver = new IntersectionObserver(easyFtn, {
+ root: null,
+ threshold: 0,
+ rootMargin: `-${topHeight}px`
+});
+easyObserver.observe(sectionOne);
+
+// WORKING WITH SCROLL REVEAL
+const allSection = document.querySelectorAll('.section')
+
+const sectionFtn = (entries) => {
+ const [entry] =entries;
+ console.log(entry)
+ if(!entry.isIntersecting) return;
+
+ entry.target.classList.remove('section-hidden')
+}
+
+const sectionScroll = new IntersectionObserver(sectionFtn, {
+ root: null,
+ threshold: 0.2,
+});
+
+allSection.forEach(el => {
+ sectionScroll.observe(el)
+ el.classList.add('section-hidden')
+})
